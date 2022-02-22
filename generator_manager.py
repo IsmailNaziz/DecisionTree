ba5ddb415ca3f730c1332b6addc304b9cd3e073b
morokhalid16 <morokhalid16@gmail.com>
2022-02-22 23:23:01 +0100

    adding number of rows and column names attributes for generator_manager class

--- generator_manager.py
+++ generator_manager.py
@@ -1,4 +1,6 @@
 import os
+import random
+from numpy import random
 import pandas as pd
 class GeneratorManager(object):
     '''
@@ -25,15 +27,19 @@ class GeneratorManager(object):
 
     you need to implement the method run()
     '''
-    def __init__(self,csv_name,n_columns):
+    def __init__(self,csv_name,n_columns,n_rows=0,col_names=[]):
         self.csv_name=csv_name
         self.n_columns=n_columns
+        self.n_rows=n_rows
         self.col_names=['column {}'.format(i) for i in range(self.n_columns)]
     def set_col_names(self, col_names):
+        assert (len(col_names)==self.n_columns), 'Your col_name list should have the same size as the number of columns'
         self.col_names=col_names
     def run(self):
         self.set_col_names(self.col_names)
         df = pd.DataFrame(columns=self.col_names)
+        for col in self.col_names:
+            df[col]=random.rand(self.n_rows)
         os.makedirs('..\DecisionTree\inputs', exist_ok=True)
         df.to_csv('..\DecisionTree\inputs\ '+self.csv_name)
 
@@ -43,6 +49,7 @@ class GeneratorManager(object):
 
 
 if __name__=='__main__':
-    parameters_set = {'csv_name':'out','n_columns':3}
+    parameters_set = {'csv_name':'out','n_columns':3,'n_rows':10}
     GM = GeneratorManager(**parameters_set)
+    GM.set_col_names(['A','B','C'])
     GM.run()

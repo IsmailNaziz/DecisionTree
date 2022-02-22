b43902d2e92bdcd2408af533b0644a46ea05a7ed
morokhalid16 <morokhalid16@gmail.com>
2022-02-22 01:14:26 +0100

    add constructor

--- generator_manager.py
+++ generator_manager.py
@@ -1,3 +1,5 @@
+import os
+import pandas as pd
 class GeneratorManager(object):
     '''
     Features:
@@ -23,9 +25,24 @@ class GeneratorManager(object):
 
     you need to implement the method run()
     '''
-    pass
+    def __init__(self,csv_name,n_columns):
+        self.csv_name=csv_name
+        self.n_columns=n_columns
+        self.col_names=['column {}'.format(i) for i in range(self.n_columns)]
+    def set_col_names(self, col_names):
+        self.col_names=col_names
+    def run(self):
+        self.set_col_names(self.col_names)
+        df = pd.DataFrame(columns=self.col_names)
+        os.makedirs('..\DecisionTree\inputs', exist_ok=True)
+        df.to_csv('..\DecisionTree\inputs\ '+self.csv_name)
+
+        return
+
+
+
 
 if __name__=='__main__':
-    parameters_set = {}
+    parameters_set = {'csv_name':'out','n_columns':3}
     GM = GeneratorManager(**parameters_set)
     GM.run()

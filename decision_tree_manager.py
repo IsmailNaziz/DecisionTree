c85ca39e31c20e30f256c0c87aef159e8b179ab7
morokhalid16 <morokhalid16@gmail.com>
2022-03-08 01:33:20 +0100

    adding best split function

--- decision_tree_manager.py
+++ decision_tree_manager.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
+from node import Node
+
+
 class DecisionTreeEngine(object):
 
-    def __init__(self, file_name, cost_function):
-        self.file_name = file_name
-        self.df = self._read_file()
-        self.cost_function = cost_function
-        self.df_description = self._set_description_df()
+    def __init__(self, max_depth):
+        # self.file_name = file_name
+        # self.df = self._read_file()
+        # self.cost_function = cost_function
+        # self.df_description = self._set_description_df()
+        self.max_depth = max_depth
 
     def _set_description_df(self):
         ''' return dict keys:col_name value type, it can be either continuous, categorical'''
@@ -21,8 +25,8 @@ class DecisionTreeEngine(object):
         input df supposed to be compoesed of 2 columns input and output that are both boolean
         return 4 df splitted depending on booleans
         '''
-        return df[df["output"]==True & df["input"]==True], df[df["output"]==True & df["input"]==False],\
-                df[df["output"] == False & df["input"] == True], df[df["output"]==False & df["input"]==False]
+        return df[df["output"] == True & df["input"] == True], df[df["output"] == True & df["input"] == False], \
+               df[df["output"] == False & df["input"] == True], df[df["output"] == False & df["input"] == False]
 
     def calculate_gini_for_boolean_cols(self, df):
         ''' general case when input and output are both True or False
@@ -47,10 +51,28 @@ class DecisionTreeEngine(object):
         '''
         return
 
-    def select_best_split(self):
+    def select_best_split(self, x, y):
         '''ALL COLUMNS + GINI
         return threshhold + col or col'''
-        return
+        if y.size <= 1:
+            return None, None
+        best_threshold = None
+
+        for col in x.columns:
+            sorted_samples, classes = zip(*sorted(zip(x[col], y)))
+            classes = pd.DataFrame(classes)
+            best_gini = self.gini(classes)
+            for i in range(1, len(sorted_samples)):
+                gini_left = self.gini(classes[:i])
+                gini_right = self.gini(classes[i:])
+                gini = (classes[:i].size / y.size) * gini_left + (classes[i:].size / y.size) * gini_right
+                if gini <= best_gini:
+                    best_gini = gini
+                    best_feature = col
+                    best_threshold = (sorted_samples[i-1] + sorted_samples[i]) / 2
+
+        print('best threshold: {} best gini {}'.format(best_threshold, best_gini))
+        return best_feature, best_threshold
 
     def calculate_cost(self, df, input_col, output_col):
         dft = df[[input_col, output_col]]
@@ -60,10 +82,56 @@ class DecisionTreeEngine(object):
         elif self.df_description[input_col] == 'categortcal':
             return
 
-    def gini(self, p_x, p_y):
-        return
+    def gini(self, y):
 
-
+        return 1 - ((y.value_counts() / len(y)) ** 2).sum()
 
     def calculate_lowest_cost(self):
         return
+
+    def generate_tree(self, x, y, depth=0):
+        '''
+
+        :param depth:
+        :param df: input data
+        :return: tree
+        '''
+
+        node = Node()
+        if depth < self.max_depth:
+            feature, tr = self.select_best_split(x, y)
+            node.feature = feature
+            node.threshold = tr
+            df = pd.concat([x, y], axis=1)
+            df_left = df[df[feature] <= tr]
+            df_right = df[df[feature] >= tr]
+            node.left = self.generate_tree(df_left[x.columns], df_left['target'], depth=depth +1)
+            node.right = self.generate_tree(df_right[x.columns], df_right['target'], depth=depth +1)
+        return node
+
+    def fit(self, x, y):
+
+        self.tree = self.generate_tree(x, y)
+
+
+    def predict(self, x):
+        tree = self.tree
+        while tree.left:
+            if x[tree.feature] <= tree.threshold:
+                tree = tree.left
+            else:
+                tree = tree.right
+        return tree.predicted_class
+
+
+if __name__ == '__main__':
+    clf = DecisionTreeEngine(max_depth=3)
+    df = pd.DataFrame({
+                   'points': [11, 7, 8, 10, 13, 13],
+                   'assists': [5, 7, 7, 9, 12, 9],
+                   'target': [1, 0, 0, 1, 1, 0]})
+    y = df['target']
+    x = df.drop(['target'], axis=1)
+    clf.fit(x, y)
+    #clf.select_best_split(x, y)
+
--- node.py
+++ node.py
@@ -1,1 +1,12 @@
-import pandas
+class Node(object):
+
+    def __int__(self, predicted_class=0):
+        self.feature = ''
+        self.left = None
+        self.right = None
+        self.predicted_class = predicted_class
+
+
+
+
+
